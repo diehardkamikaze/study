0d08fd70141e2707665fab63660aa195b7a3bae2
diehardkamikaze <diehardkamikaze@gmail.com>
2021-01-30 04:49:20 +0300

    useState!

--- React/react-youtube/src/Todo/TodoItem.js
+++ React/react-youtube/src/Todo/TodoItem.js
@@ -1,11 +1,36 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-function TodoItem({ todo, index }) {
+const styles= {
+    li: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: '.5rem 1rem',
+        border: '1px solid grey',
+        borderRadius: '4px',
+        marginBottom: '.5rem',  
+    },
+    input: {
+        marginRight: '1rem',
+    }
+}
+
+function TodoItem({ todo, index, onChange }) {
+    const classes = [];
+
+    if(todo.completed)
+        classes.push('done');
+
     return (
-        <li>
-            <strong>{index + 1}</strong>
-            {todo.title}
+        <li style={styles.li}>
+            <span className={classes.join(' ')}>
+                <input style={styles.input} checked={todo.completed} type="checkbox" onChange={() => onChange(todo.id)}/>
+                <strong>{index + 1}</strong>
+                &nbsp;
+                {todo.title}
+            </span>
+            <button className='rm'>&times;</button>
         </li>
     );
 }
@@ -13,6 +38,7 @@ function TodoItem({ todo, index }) {
 TodoItem.propTypes = {
     todo: PropTypes.object.isRequired,
     index: PropTypes.number,
+    onChange: PropTypes.func.isRequired,
 }
 
 export default TodoItem
