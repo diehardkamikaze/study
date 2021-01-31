7a232ff053776adb35cc8f076aa8167ae400bc3b
diehardkamikaze <diehardkamikaze@gmail.com>
2021-01-31 05:26:24 +0300

    added primitive addtodo form

--- React/react-youtube/src/App.js
+++ React/react-youtube/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import TodoList from './Todo/TodoList'
-
+import Context from './context'
+import AddTodo from './Todo/AddTodo'
 
 
 function App() {
@@ -17,12 +18,27 @@ function App() {
     }))
   }
 
+function removeTodo(id)
+{
+  setTodos(todos.filter(todo => todo.id !== id))
+}
+
+function addTodo(title)
+{
+  setTodos(todos.concat([
+     {title,
+     id: Date.now(),
+    completed: false,  }]))
+}
 
   return (
-   <div className='wrapper'>
-     <h1>React tutorial</h1>
-     <TodoList todos={todos} onToggle={toggleTodo}></TodoList>
-   </div>
+    <Context.Provider value={{removeTodo: removeTodo}}>
+      <div className='wrapper'>
+        <h1>React tutorial</h1>
+        <AddTodo onCreate={addTodo}/>
+        {todos.length ? <TodoList todos={todos} onToggle={toggleTodo}></TodoList> : <p>No todos!</p>}
+      </div>
+   </Context.Provider>
   );
 }
 
---
+++ React/react-youtube/src/Todo/AddTodo.js
@@ -0,0 +1,30 @@
+import React, {useState} from 'react'
+import PropTypes from 'prop-types'
+
+function AddTodo({onCreate})
+{
+    function submitHandler(event)
+    {
+        event.preventDefault()
+
+        if(value.trim())
+        {
+            onCreate(value)
+            setValue('')
+        }
+    }
+
+    const [value, setValue] = useState('');
+    return (
+        <form style={{marginBottom: '1rem'}} onSubmit={submitHandler}>
+            <input value={value} onChange={event => setValue(event.target.value)}  />
+            <button type="sumbit">Add todo</button>
+        </form>
+    )
+}
+
+AddTodo.propTypes = {
+    onCreate: PropTypes.func.isRequired
+}
+
+export default AddTodo
