5f1d23ddf24ebae411f3182879c8a2a469ede226
diehardkamikaze <diehardkamikaze@gmail.com>
2021-02-09 19:49:05 +0300

    hoitsing, let, const, scopes

---
+++ JS/02_values.js
@@ -0,0 +1,18 @@
+let a = 42;
+let b = a;
+b++;
+console.log('a', a);
+console.log('b', b);
+
+let c = [1, 2, 3]
+let m = c;
+m.push(4);
+console.log('c', c);
+console.log('m', m);
+//use concat, returns copy of array
+
+
+let d = [1,2,3,4];
+console.log(c == m);
+console.log(c == d);
+
