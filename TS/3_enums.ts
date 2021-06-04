30029929f2724b7c4e0c8d953bbb87028e1fc3ab
captainAlatriste <diehardkamikaze@gmail.com>
2021-06-04 17:58:23 +0300

    enums

---
+++ TS/3_enums.ts
@@ -0,0 +1,20 @@
+enum Teeest {
+  VALERA,
+  ALESHA,
+  SASHA
+}
+
+const brow = Teeest.VALERA;
+const browReverse = Teeest[0]
+console.log(brow);
+console.log(browReverse)
+
+
+enum socialMedia {
+  VK = 'vkp',
+  FB = 'fbp',
+  OK = 'okp',
+}
+
+const social = socialMedia.FB;
+console.log(social);
